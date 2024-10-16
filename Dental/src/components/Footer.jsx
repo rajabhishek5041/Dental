756971c1010cf4e57db26918fd81aa90d46af35c
Ayush <shakyaayush31@gmail.com>
2024-10-16 11:06:29 +0530

    navbar changes

---
+++ Dental/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h1 className='h-[300px] w-full  bg-blue-300 text-center text-3xl font-bold ' >This is Footer </h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Dental/src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div>
+      <h1 className='h-screen font-bold text-3xl text-center ' > This ia About us page  </h1>
+    </div>
+  )
+}
+
+export default AboutUs
